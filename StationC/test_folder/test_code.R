b24cc4b8a92bdded4489078907f6cb875e49c881
isamarcortes <cortesi1@montclair.edu>
2023-06-20 14:36:50 -0400

    Added more to test code

--- StationC/test_folder/test_code.R
+++ StationC/test_folder/test_code.R
@@ -1,6 +1,17 @@
-plot_info <- read.csv(‘plotdata.csv’)
-agb <- raste(“agb_raster.tif’)
+library('raster')
 
-coordinates(plot_info) = ~ lon + lat
-crs(plot_info) <- crs(agb)
-Plot_agb <- raster::extract(agb, plot_info[,1:2])
+setwd('/Users/isamarcortes/Documents/SPEC_School/StationC')###change this to your local repo
+
+KamoskeData <- read.csv('Kamoske_etal_2022_data/all_metrics_20200803.csv')###csv file
+agb <- raster('MLBS_agbEcoregion_20m.tif')###agb raster
+plot(agb)
+
+shapefile <- st_read('Kamoske_etal_2022_data/neon_plots_mlbs.shp')###shapefile data
+plot(shapefile,add=T)
+
+shapefileAGB <- as.data.frame(extract(agb,shapefile,fun=mean))###taking average for each shapefile
+shapefileAGB$plot <- shapefile$id ###adds ID to shapefileAGB
+colnames(shapefileAGB)<- c("Biomass Raster","plotID")
+newDF <- merge(KamoskeData,shapefileAGB,by='plotID')
+
+plot()
