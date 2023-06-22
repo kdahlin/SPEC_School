bf1aff1d542424dd612e043630886db60e4fd3e8
Kathryn Wheeler <kiwheel@bu.edu>
2023-06-22 09:51:29 -0400

    Finished writing code to for downloading lidar, filtering, and plotting

--- StationG/lidarData_KW.R
+++ StationG/lidarData_KW.R
@@ -1,5 +1,8 @@
 library('neonUtilities')
 library('neonOS')
+library('lidR')
+#devtools::install_github("akamoske/canopyLazR")
+library('canopyLazR')
 dpID <- "DP1.30003.001"
 siteID <- "MLBS"
 setwd("StationG")
@@ -9,3 +12,12 @@ byTileAOP(dpID=dpID,
           easting=542067,
           northing=4136943,
           check.size = FALSE) 
+
+lidarFileName <- "DP1.30003.001/neon-aop-products/2021/FullSite/D07/2021_MLBS_4/L1/DiscreteLidar/ClassifiedPointCloud/NEON_D07_MLBS_DP1_542000_4136000_classified_point_cloud_colorized.laz"
+lidarDat <- readLAS(lidarFileName)
+plot(lidarDat) #Three "levels" appear and the top and bottom are noise
+
+lidarDat_filtered <- filter_poi(lidarDat,Z>1100, Z<1250) #Z is m above sea level
+  
+plot(lidarDat_filtered) #Not looking as clean as if you clipped it on cloudcompare
+
