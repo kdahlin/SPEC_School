11cbd91222cae599147ed1ee5c633335ba25e0ae
melferriter <mferriter@ucla.edu>
2024-06-11 10:33:11 -0700

    test

--- spec_school_2024/plsr-hyperspectral/SPEC_School_2023_PLSR.R
+++ spec_school_2024/plsr-hyperspectral/SPEC_School_2023_PLSR.R
@@ -1,5 +1,5 @@
 ################################################################################
-#test
+#testm
 # Partial Least Squares Regression (PLSR) code for exploring leaf traits with 
 # hyperspectral data ***USING handheld spectra***
 
