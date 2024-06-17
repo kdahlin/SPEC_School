bf076447d9a6a749e124ddeb475ed499798a90e0
Kyla D <kyla.dahlin@gmail.com>
2024-06-16 21:36:57 -0400

    reading in hdf5 files and plotting

--- spec_school_2024/hsi-imagery/hsi_imagery_exploration.R
+++ spec_school_2024/hsi-imagery/hsi_imagery_exploration.R
@@ -1,38 +1,37 @@
 ################################################################################
-### extracting reflectance data for each GPS location (with multiple flightlines
-### per GPS location) ###
+### looking at reflectance data tiles and making some plots
 ### Drafted by Kyla Dahlin (kdahlin@msu.edu)
+### modified from hypRspec (Kamoske)
 ### last updated 20240616
 ################################################################################
+#install.packages("BiocManager")
 
-
+library(BiocManager)
+#BiocManager::install("rhdf5")
 
 library(rhdf5)
-library(raster)
-library(rgdal)
+library(terra)
 options(digits = 20)
 
 # this code will produce a csv with a row for each point x flightline combo
 # where do you want to save that info?
-out.dir <- "C:/Users/kdahlin/Dropbox/NEON_hsi_lidar/hsi_lidar_ms1/data/"
+out.dir <- "K:/SPEC_School_2024/HSI_image_practice/"
 today <- format(Sys.Date(), "%Y%m%d")
-loc <- "HARV"
+loc <- "MLBS"
 
 # where to find stuff
-hsi.data.dir <- paste0("X:/shared_data/NEON_AOP_data/",loc,"/2017/HSI/")
+hsi.data.dir <- paste0("Z:/shared_data/NEON_AOP_data/MLBS/2022/NEON_refl-surf-",
+                       "bidir-ortho-mosaic/NEON.D07.MLBS.DP3.30006.002.2022-09.",
+                       "basic.20240530T173638Z.PROVISIONAL/")
 
-# get the file produced in the prev r script
-pointsXflights <- read.csv(paste0(out.dir,loc,"_pointsXflights_20181120.csv"), 
-                           stringsAsFactors = FALSE)
+# get hsi filenames
+hsi.files <- list.files(hsi.data.dir)
 
-flights <- unique(pointsXflights$flightline)
-
-# list all of the data types within the hdf5 (this should be the same for all files)
-h5ls(file = paste0(hsi.data.dir, flights[1]))
-
+# list all of the data types within the hdf5 (should be the same for all files)
+h5ls(file = paste0(hsi.data.dir, hsi.files[2]))
 
 # get a list of wavelengths (should be the same for all files)
-wavelengths <- h5read(file = paste0(hsi.data.dir, flights[1]),
+wavelengths <- h5read(file = paste0(hsi.data.dir, hsi.files[2]),
                       name = paste0(loc, 
                                     "/Reflectance/Metadata/Spectral_Data/Wavelength"))
 wave.count <- 1:length(wavelengths)
@@ -46,103 +45,86 @@ write.csv(out.waves,
           paste0(out.dir, "NEON_HSI_wavelengths_", today, ".csv"),
           row.names = FALSE)
 
-# make an output table
-out.data <- as.data.frame(matrix(data = NA, 
-                                 nrow = 0, 
-                                 ncol = (length(wave.names) + 4)), 
-                                 stringsAsFactors = FALSE)
+# let's get the projection info too
+in.proj <- h5read(file = paste0(hsi.data.dir, hsi.files[2]),
+                      name = paste0("/MLBS/Reflectance/Metadata/",
+                      "Coordinate_System/EPSG Code"))
 
-names(out.data) <- c("pointIDs", 
-                     "easting", 
-                     "northing", 
-                     "flightline", 
-                     wave.names)
+# and let's fine the lower left easting and northing values
+in.info <- h5read(file = paste0(hsi.data.dir, hsi.files[2]),
+                  name = paste0("/MLBS/Reflectance/Metadata/",
+                                "Coordinate_System/Map_Info"))
+in.info.sep <- strsplit(in.info, ",")
 
-for (i in 1:length(flights)) {
-  # get the coordinate system
-  coords <- h5read(file = paste0(hsi.data.dir, flights[i]),
-                   name = paste0(loc, "/Reflectance/Metadata/Coordinate_System"))
-  
-  map.info <- strsplit(coords$Map_Info, 
-                       split = ",", 
-                       fixed = T)
-  easting.ul <- as.numeric(map.info[[1]][4])
-  northing.ul <- as.numeric(map.info[[1]][5])
-  
-  locs.subset <- subset(pointsXflights, 
-                        pointsXflights$flightline == flights[i])
-  locs.subset <- locs.subset[,-5]
-  
-  out.subset <- as.data.frame(matrix(data = NA, 
-                                     nrow = dim(locs.subset[1]), 
-                                     ncol = (length(wave.names) + 4)), 
-                              stringsAsFactors = FALSE)
-  names(out.subset) <- c("pointIDs", 
-                         "easting", 
-                         "northing", 
-                         "flightline", 
-                         wave.names)
-  
-  out.subset[,1:4] <- locs.subset
-  
-  # now open up each refl band and extract points
-  for (j in 1:length(wavelengths)) {
-    # get subsetted reflectance
-    refl.sub <- h5read(file = paste0(hsi.data.dir, flights[i]),
-                       name = paste0(loc, "/Reflectance/Reflectance_Data"),
-                       index = list(j, NULL, NULL))
-    
-    refl.stack <- raster(as.matrix(refl.sub[1,,]))
-    
-    refl.stack[refl.stack == -9999] <- NA
-    refl.stack.t <- t(refl.stack)
-    
-    line.dim <- dim(refl.stack.t)
-    y.dim <- line.dim[1]
-    x.dim <- line.dim[2]
-    
-    extent(refl.stack.t) <- c(easting.ul, 
-                              easting.ul+x.dim, 
-                              northing.ul-y.dim, 
-                              northing.ul)
-    
-    # assign projection (note - this is hard coded because it's 'UTM' in the hdf5 
-    #file and it's acutally case sensitive and should be 'utm' :[ sigh.)
-    projection(refl.stack.t) <- "+proj=utm +zone=18 +north 
-                                 +datum=WGS84 +units=m +no_defs"
-    
-    out.vals <- extract(refl.stack.t, 
-                        as.matrix(out.subset[,2:3]), 
-                        method = "simple")
-    
-    out.subset[,4+j] <- out.vals
-    print(paste(j, date()))
-  }
+ll.x <- as.numeric(in.info.sep[[1]][4])
+ll.y <- as.numeric(in.info.sep[[1]][5])
 
-  out.data <- rbind(out.data, out.subset)
-  print(paste("done with ", i, date()))
-}
+# let's try to make an RGB image. Let's say red is ~650 nm, green is ~550, and
+# blue is ~450
 
-out.nas <- is.na(out.data$wave.384) == F
+# find the red band
+red.index <- which(abs(wavelengths - 650) == min(abs(wavelengths - 650)))
 
-out.keep <- subset(out.data, out.nas)
+red.array <- h5read(file = paste0(hsi.data.dir, hsi.files[2]),
+                    name = "/MLBS/Reflectance/Reflectance_Data",
+                    index = list(red.index, 1:1000, 1:1000))
 
-write.csv(out.keep, 
-          paste0(out.dir, loc, "_hsi_by_point_flightline_", today, ".csv"),
-          row.names = FALSE)
+red.matrix <- red.array[1,,]
+red.rast <- rast(red.matrix,
+                 crs = crs(paste0("epsg:", in.proj)), 
+                 extent = c(ll.x, ll.x + 1000, ll.y, ll.y+1000))
+
+# now green
+green.index <- which(abs(wavelengths - 550) == min(abs(wavelengths - 550)))
+
+green.array <- h5read(file = paste0(hsi.data.dir, hsi.files[2]),
+                    name = "/MLBS/Reflectance/Reflectance_Data",
+                    index = list(green.index, 1:1000, 1:1000))
+
+green.matrix <- green.array[1,,]
+green.rast <- rast(green.matrix,
+                 crs = crs(paste0("epsg:", in.proj)), 
+                 extent = c(ll.x, ll.x + 1000, ll.y, ll.y+1000))
+
+# now blue
+blue.index <- which(abs(wavelengths - 450) == min(abs(wavelengths - 450)))
+
+blue.array <- h5read(file = paste0(hsi.data.dir, hsi.files[2]),
+                      name = "/MLBS/Reflectance/Reflectance_Data",
+                      index = list(blue.index, 1:1000, 1:1000))
+
+blue.matrix <- blue.array[1,,]
+blue.rast <- rast(blue.matrix,
+                   crs = crs(paste0("epsg:", in.proj)), 
+                   extent = c(ll.x, ll.x + 1000, ll.y, ll.y+1000))
+
+# stack them up!
+rgb.stack <- c(red.rast, green.rast, blue.rast)
+
+# and plot! 
+plotRGB(rgb.stack)
+
+# now let's make a CIR image
+
+# first we have to get a NIR layer
+nir.index <- which(abs(wavelengths - 750) == min(abs(wavelengths - 750)))
+
+nir.array <- h5read(file = paste0(hsi.data.dir, hsi.files[2]),
+                     name = "/MLBS/Reflectance/Reflectance_Data",
+                     index = list(nir.index, 1:1000, 1:1000))
+
+nir.matrix <- nir.array[1,,]
+nir.rast <- rast(nir.matrix,
+                  crs = crs(paste0("epsg:", in.proj)), 
+                  extent = c(ll.x, ll.x + 1000, ll.y, ll.y+1000))
+
+# make a NIR stack
+nir.stack <- c(nir.rast, red.rast, green.rast)
+
+# and plot!
+plotRGB(nir.stack)
 
 
-### making a plot just to make me happy
 
-x11()
-plot(wave.round, c(0, rep(10000, length(wave.round)-1)), type = "n")
-lines(wave.round, out.keep[25,5:430])
-lines(wave.round, out.keep[150,5:430])
-lines(c(397,397), c(0,10000), col = "red")
-lines(c(1352,1352), c(0,10000), col = "red")
-lines(c(1428,1428), c(0,10000), col = "red")
-lines(c(1808,1808), c(0,10000), col = "red")
-lines(c(1963,1963), c(0,10000), col = "red")
-lines(c(2473,2473), c(0,10000), col = "red")
 
 
