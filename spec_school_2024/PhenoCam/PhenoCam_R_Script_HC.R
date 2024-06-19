f1f01017b714e256aebeba71bcd582db5f83308e
HANSSSY <111121689+HANSSSY@users.noreply.github.com>
2024-06-19 14:39:27 -0400

    Add time series plot

--- spec_school_2024/PhenoCam/PhenoCam_R_Script_HC.R
+++ spec_school_2024/PhenoCam/PhenoCam_R_Script_HC.R
@@ -11,7 +11,21 @@ library(jpeg)
 phenos <- get_phenos()
 
 # to obtain the DB 1000 from dukehw
-MLBS_under_UN_1000 <- get_pheno_ts(site = 'NEON.D07.MLBS.DP1.00042', vegType = 'UN', roiID = 1000, type = 'roistats')
+MLBS42_UN_1000 <- get_pheno_ts(site = 'NEON.D07.MLBS.DP1.00042', vegType = 'UN', roiID = 1000, type = '3day')
+View(MLBS42_UN_1000)
+# date variable into date format
+MLBS42_UN_1000[,date:=as.Date(date)]
+# plot gcc_90
+MLBS42_UN_1000[,plot(date, gcc_90, col = 'green', type = 'b')]
+
+#> NULL
+
+mtext('Duke Forest, Hardwood', font = 2)
+
+
+
+MLBS_under_UN_1000 <- get_pheno_ts(site = 'NEON.D07.MLBS.DP1.00042', vegType = 'UN', roiID = 1000, type = '3day')
+View(MLBS_under_UN_1000)
 
 ??get_pheno_ts
 NEON.D07.MLBS.DP1.00042
