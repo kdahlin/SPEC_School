670acd556a6379d2869d120841aa5ef14736d3e6
Ireland <126912033+irelandsherrill@users.noreply.github.com>
2024-06-19 11:54:26 -0700

    Add files via upload

---
+++ spec_school_2024/Hemisphere_Calculation.R
@@ -0,0 +1,11 @@
+# SPEC School 2024
+# 6/19/2024
+# Rotating exercises - Tony Station with NEON Hemisphere Data for estimating LAI
+# File path on mapped drive -> Z:\shared_data\NEON_field_data\MLBS\2020_NEON_hemispheric-photos-veg
+
+# Set working directory
+setwd("Z:/shared_data/NEON_field_data/MLBS/2020_NEON_hemispheric-photos-veg")
+
+# Install R packages to process hemispherical photos
+install.packages("hemispheR")
+library(hemispheR)
