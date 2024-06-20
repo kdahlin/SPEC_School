ed5e80736761feaf82b42dffca7b2f73d8bb8b25
HANSSSY <111121689+HANSSSY@users.noreply.github.com>
2024-06-20 14:42:17 -0400

    updates

--- spec_school_2024/Phenology_climate_project/PhenologyClimate_codes.R
+++ spec_school_2024/Phenology_climate_project/PhenologyClimate_codes.R
@@ -6,8 +6,51 @@ library(phenocamapi)
 library(lubridate)
 library(jpeg)
 
-####testing###########################
-###test for the github pull, commit, and pull
+setwd("E:\\Prof. Qiu Lab\\2024SPECschool\\SPEC_School\\spec_school_2024\\Phenology_climate_project")
+
+#####test for pull, commitxxxxx
+####Hanshi will process the PhenoCam data (time series: 1 day)
+####The time covers 2018-2022
+##obtain the phenoCam data of MLBS (understory and overstory)
+# obtaining the phenocam site metadata from the server as data.table
+phenos <- get_phenos()
+# to obtain the UN 1000 from MLBS
+MLBS_under_UN_1000 <- get_pheno_ts(site = 'NEON.D07.MLBS.DP1.00042', vegType = 'UN', roiID = 1000, type = 'roistats')
+View(MLBS_under_UN_1000)
+MLBS_over_DB_2000 <- get_pheno_ts(site = 'NEON.D07.MLBS.DP1.00033', vegType = 'DB', roiID = 2000, type = 'roistats')
+View(MLBS_over_DB_2000)
+
+##Date cleaning
+# Remove columns that contain only NA values
+MLBS_under_UN_1000 <- MLBS_under_UN_1000 %>%
+  select_if(~ !all(is.na(.)))
+
+# Define the column names
+colnames(MLBS_under_UN_1000) <-   c("date", "local_std_time", "doy", "filename",
+                                                "solar_elev", "exposure", "awbflag", "mask_index",
+                                                "gcc", "rcc", "r_mean",	"r_std",
+                                                "r_5_qtl","r_10_qtl",	"r_25_qtl",	"r_50_qtl",
+                                                "r_75_qtl","r_90_qtl","r_95_qtl", "g_mean",
+                                                "g_std","g_5_qtl","g_10_qtl","g_25_qtl","g_50_qtl",
+                                                "g_75_qtl","g_90_qtl","g_95_qtl","b_mean","b_std",
+                                                "b_5_qtl","b_10_qtl","b_25_qtl","b_50_qtl",	"b_75_qtl",
+                                                "b_90_qtl","b_95_qtl","r_g_correl","g_b_correl","b_r_correl")
+# Convert the date column to Date type
+MLBS_under_UN_1000$date <- as.Date(MLBS_under_UN_1000$date, format="%Y-%m-%d")
+# Filter the data from 2018 to 2022
+filtered_under <- MLBS_under_UN_1000 %>%
+  filter(date >= as.Date("2018-01-01") & date <= as.Date("2022-12-31"))
+View(filtered_under)
+
+##Aggregate the data with the same dates into mean value
+# Extract month and day from the date column
+filtered_under <- filtered_under %>%
+  mutate(month_day = format(date, "%m-%d"))
+# Group by month and day and calculate the mean of the GCC column
+aggregated_under <- filtered_under %>%
+  group_by(month_day) %>%
+  summarize(gcc = mean(gcc, na.rm = TRUE))
+View(aggregated_under)
 
 ##obtain the trait of MLBS
 MLBS_trait <- loadByProduct(dpID="DP1.10026.001", 
@@ -31,12 +74,7 @@ MLBS_intensity <- MLBS_Gphenology$phe_statusintensity
 MLBS_combined <- left_join(MLBS_perindividual, MLBS_intensity, by = "individualID")
 View(MLBS_combined)
 
-##obtain the phenoCam data of MLBS (understory and overstory)
-# obtaining the phenocam site metadata from the server as data.table
-phenos <- get_phenos()
 
-# to obtain the DB 1000 from dukehw
-MLBS_under_UN_1000 <- get_pheno_ts(site = 'NEON.D07.MLBS.DP1.00042', vegType = 'UN', roiID = 1000, type = 'roistats')
 
 ??get_pheno_ts
 NEON.D07.MLBS.DP1.00042
