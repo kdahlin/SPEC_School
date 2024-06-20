20b602e7899029d6c2f3eb612e9eaa4a87a88b06
Ben Carlson <ben.s.carlson@gmail.com>
2024-06-20 14:53:57 -0400

    download and aggregate ir bio temp

---
+++ spec_school_2024/Phenology_climate_project/IR_bio_temp.r
@@ -0,0 +1,54 @@
+
+#Downloads IR_bio_temp product and aggregates to daily min, mean, max
+# Follows this tutorial: https://www.neonscience.org/resources/learning-hub/tutorials/neon-saat-temp-r
+
+suppressWarnings(
+  suppressPackageStartupMessages({
+    library(neonUtilities)
+    library(tidyverse)
+  }))
+
+irbioPkg <- loadByProduct(dpID="DP1.00005.001", site="MLBS", 
+                      startdate="2018-01", enddate="2022-12",
+                      package="basic", timeIndex="30",
+                      check.size = F)
+
+View(irbioPkg)
+
+list2env(irbioPkg, .GlobalEnv)
+
+View(IRBT_30_minute)
+
+irbio <- as_tibble(IRBT_30_minute)
+
+unique(irbio$verticalPosition) #Check what vertical positions are available
+
+irbioAgg <- irbio %>%
+  filter(verticalPosition %in% c('000','040') &
+        finalQF==0 &
+        !is.na(bioTempMean)) %>%
+  mutate(doy=yday(startDateTime),
+         year=year(startDateTime)) %>%
+  group_by(verticalPosition,year,doy) %>%
+  summarize(
+    t_mean=mean(bioTempMean),
+    t_min=min(bioTempMean),
+    t_max=max(bioTempMean)
+  ) %>%
+  ungroup %>%
+  arrange(verticalPosition,year,doy)
+
+hpccPath <- '/Volumes/ersamlab/phenology/data'
+
+#Save top-of-canopy temperatures
+irbioAgg %>%
+  filter(verticalPosition=='000') %>%
+  select(-verticalPosition) %>%
+  write_csv(file.path(hpccPath,'top_irbio_temperature.csv'))
+
+#Save top-of-canopy temperatures
+irbioAgg %>%
+  filter(verticalPosition=='040') %>%
+  select(-verticalPosition) %>%
+  write_csv(file.path(hpccPath,'bottom_irbio_temperature.csv'))
+  
