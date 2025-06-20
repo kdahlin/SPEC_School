65aae602af300fd015f11d771f278a5a2474a3ee
AbbyMB14 <106114601+AbbyMB14@users.noreply.github.com>
2025-06-20 12:50:06 -0400

    more models

--- spec_school_2025/leaf_level_spectra/modelmaybe.R
+++ spec_school_2025/leaf_level_spectra/modelmaybe.R
@@ -5,6 +5,11 @@ getwd()
 setwd("C:/Desktop/Spec2025/SPEC_School/spec_school_2025/leaf_level_spectra")
 weights<-read.csv("weight.only.csv")
 
+library(MASS)
+library(ggplot2)
+library(ggpmisc)
+library(tidyverse)
+
 
 averaged_spectra <- spectra %>%
   group_by(species, timestep, temp) %>%
@@ -13,11 +18,12 @@ averaged_spectra <- spectra %>%
 averaged_spectra<-averaged_spectra[-2,]
 averaged_spectra<-averaged_spectra[-1,]
 
+write.csv(averaged_spectra, "averaged_spectra.csv")
+
+averaged_spectra<-read.csv("averaged_spectra.csv")
+
 hist(averaged_spectra$avg_value)
 
-library(MASS)
-library(ggplot2)
-library(ggpmisc)
 rsq <- function (x, y) cor(x, y) ^ 2
 
 #first model
@@ -45,20 +51,72 @@ vec2<-two$fitted.values
 
 averaged_spectra$vec2<-vec2
 
+rsq(averaged_spectra$avg_value, averaged_spectra$vec2)
+
 ggplot(data = averaged_spectra, aes(x = avg_value, y = vec2)) +
   geom_point() +
-  stat_smooth(method = "lm", se = TRUE, color="violet") +
+  stat_smooth(method = "lm", se = TRUE, color = "orange") +
   stat_poly_eq(
     aes(label = paste(..rr.label..)),
     formula = y ~ x,
     parse = TRUE,
     size = 5
   ) +
-  labs(x = "Measured", y = "Predicted", title="Reflectance Model") +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "gray40") +
+  labs(x = "Measured", y = "Predicted", title = "Reflectance Model") +
+  theme_bw()
+
+#third model
+hist(averaged_spectra$LWC)
+three<-glm(LWC~avg_value+species+timestep+temp, data=averaged_spectra)
+
+plot(three$residuals)
+summary(three)
+
+vec3<-three$fitted.values
+
+averaged_spectra$vec3<-vec3
+
+rsq(averaged_spectra$avg_value, averaged_spectra$vec3)
+
+ggplot(data = averaged_spectra, aes(x = LWC, y = vec3)) +
+  geom_point() +
+  stat_smooth(method = "lm", se = TRUE, color = "green") +
+  stat_poly_eq(
+    aes(label = paste(..rr.label..)),
+    formula = y ~ x,
+    parse = TRUE,
+    size = 5
+  ) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "gray40") +
+  labs(x = "Measured", y = "Predicted", title = "LWC Model") +
   theme_bw()
 
 
-rsq(averaged_spectra$avg_value, averaged_spectra$vec2)
+#fourth model
+hist(averaged_spectra$RWC)
+four<-glm(RWC~avg_value+species+timestep+temp, data=averaged_spectra)
 
+plot(four$residuals)
+summary(four)
+
+vec3<-three$fitted.values
+
+averaged_spectra$vec3<-vec3
+
+rsq(averaged_spectra$avg_value, averaged_spectra$vec3)
+
+ggplot(data = averaged_spectra, aes(x = LWC, y = vec3)) +
+  geom_point() +
+  stat_smooth(method = "lm", se = TRUE, color = "green") +
+  stat_poly_eq(
+    aes(label = paste(..rr.label..)),
+    formula = y ~ x,
+    parse = TRUE,
+    size = 5
+  ) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "gray40") +
+  labs(x = "Measured", y = "Predicted", title = "LWC Model") +
+  theme_bw()
 
 
